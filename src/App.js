1c76166d20dc6e83ebd151891ded2b3b6b20acf5
Kyle G <legokyle@frontier.com>
2021-03-26 14:45:30 -0700

    updated structure of app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
-import HomePage from "./components/HomePage";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AboutMe from "./components/AboutMe";
+import ContactPage from "./components/Contact";
+import PortfolioPage from './components/Portfolio';
+import NavTabs from './components/NavTabs'
 
 function App() {
-  return <HomePage />;
+  return (
+    <Router>
+      <div>
+        <NavTabs />
+        <Route exact path='/' component={AboutMe} />
+        <Route exact path='/portfolio' component={PortfolioPage} />
+        <Route exact path='/contact' component={ContactPage} />
+      </div>
+    </Router>
+  )
 }
 
 export default App;
