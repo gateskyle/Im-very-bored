0866bcaa135a5b5d6a5ed1877b9b51ccae47b4c1
Kyle G <legokyle@frontier.com>
2021-03-26 16:07:51 -0700

    Final upate to App.js file

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import AboutMe from "./components/AboutMe";
 import ContactPage from "./components/Contact";
 import PortfolioPage from './components/Portfolio';
-import NavTabs from './components/NavTabs'
+import ProjectOnePage from './components/Project-1';
+import ProjectTwoPage from './components/Project-2';
+import QuizGamePage from './components/QuizGame';
+import ResumePage from './components/Resume'
+import NavTabs from './components/NavTabs';
 
 function App() {
   return (
@@ -12,7 +16,11 @@ function App() {
         <NavTabs />
         <Route exact path='/' component={AboutMe} />
         <Route exact path='/portfolio' component={PortfolioPage} />
+        <Route exact path='/portfolio/enterainment-list' component={ProjectOnePage} />
+        <Route exact path='/portfolio/dnd-helper' component={ProjectTwoPage} />
+        <Route exact path='/portfolio/quiz-game' component={QuizGamePage} />
         <Route exact path='/contact' component={ContactPage} />
+        <Route exact path='/contact/resume' component={ResumePage} />
       </div>
     </Router>
   )
