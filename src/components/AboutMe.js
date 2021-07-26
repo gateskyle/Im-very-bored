321596887562cc379b6bc93c12e03aaa72e9442f
Kyle G <legokyle@frontier.com>
2021-07-26 13:06:20 -0700

    Updating styling and info

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -17,18 +17,29 @@ function AboutMe() {
 
             <section className="row no-gutters">
                 <article className="col-sm-5">
-                    <img src={ProfilePicture} alt="Kyle Gates" className="img-fluid"></img>
+                    <img src={ProfilePicture} alt="Kyle Gates" className="img-fluid aboutMePhoto"></img>
                 </article>
 
                 <article className="col-sm">
-                    <p>Hi there, my name is Kyle Gates. I am 22 years old and currently attending the University of Oregon's coding bootcamp. I graduated from Cor Deo Christian Academy in 2017, and also have attended PCC.
-                    One reason I am taking the University of Oregon's coding bootcamp is that I love computers, and have always had a keen interest in coding, which is what I was taking at PCC.
-                    I also took two summers in-between terms at PCC to work at Fred Meyers as a Gas Attendent, were I was promoted to work the cash register at the fuel station.</p>
+                    <p>Web developer focusing on teamwork and inclusivity. Recently earned a full-stack developer certificate from the University of Oregon, which developed skills in HTML, CSS, JavaScript, JQuery, Node, MySQL, Sequelize, MongoDB, Mongoose, and React. Previous schooling included Python, Ruby, and C++. As well as prior work experience to communicate clearly and smoothly as a group.
+                    <br></br>
+                    <br></br>
+                    Technical Skills
+                    <br></br>
+                    Programming languages: Intermediate knowledge of HTML, CSS, JavaScript, JQuery, Node, MySQL, Sequelize, MongoDB, Mongoose, and React. Basic knowledge of Python, Ruby, and C++.
+                    <br></br>
+                    IDE: Visual Studio Code
+                    <br></br>
+                    OS Environment: Windows 7/10
+                    <br></br>
+                    Software Packages: Working knowledge of Microsoft Office (Word, PowerPoint, Outlook)
+                    <br></br>
+                    Computer Hardware: Built PC desktop computer system from components for programming and general use.</p>
                     
                 </article>
             </section>
 
-            <section className="row no-gutters">
+            {/* <section className="row no-gutters">
                 <article className="col-md">
                     <p>Some of my hobbies are video games and skeet shooting. I have been playing videos games since I was young, and was also a reason I am so interested in coding. My hobby of Skeet Shooting developed when
                     I was in high school, were I joined a small league in order to get some electives, and continued to do some more skeet shooting afterwards.<br></br>                    
@@ -36,25 +47,7 @@ function AboutMe() {
                     very well, which made listening about it very intriguing. My interest in military equipment was due to the fact that in times of war, military technology advanced rapidly, which greatly benefitted civllian life after wars.</p>
                 </article>
 
-            </section>
-
-            <section className="row no-gutters">
-                <article className="col-sm">
-                    <h1>Goals:</h1>
-                    <ul>
-                        <li>
-                            <p>To learn as much as I can from this bootcamp.</p>
-                        </li>
-                        <li>
-                            <p>To continue to learn code after the bootcamp.</p>
-                        </li>
-                        <li>
-                            <p>To either get a job in or start a company that specializes in coding.</p>
-                        </li>
-                    </ul>
-                </article>
-            </section>
-                
+            </section>                 */}
         </section>
     </>
   );
